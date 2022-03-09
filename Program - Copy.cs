c684635a9a7e4455b8050bcacb38328cf67ec36f
User <tu7dsvbpa@code.edu.az>
2022-03-09 12:18:55 +0400

    added Program.cs

---
+++ Program - Copy.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace task2coded
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Operator();
+        }
+        #region
+        public static void Operator()
+        {
+            Console.WriteLine("Enter the num1: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the num2: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter an option");
+            Console.WriteLine("+ : Add");
+            Console.WriteLine("- : Subtract");
+            Console.WriteLine("* : Multiply");
+            Console.WriteLine("/ : Divide");
+            int result = 0;
+            switch (Console.ReadLine())
+            {
+                case "+":
+                    result = num1 + num2;
+                    Console.WriteLine($"Your result: {num1} + {num2} = " + result);
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    Console.WriteLine($"Your result: {num1} - {num2} = " + result);
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    Console.WriteLine($"Your result: {num1} + {num2} = " + result);
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    Console.WriteLine($"Your result: {num1} + {num2} = " + result);
+                    break;
+                default:
+                    Console.WriteLine("Please, enter the number!");
+                    break;
+            }
+            Console.WriteLine();
+
+
+        }
+    }
+}
+#endregion
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Comparison();
+        }
+         
+        public static void Comparison()
+        {
+            Console.WriteLine("Enter Intermediate");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num < 50)
+            {
+                for (int i =1; i <= num; i++)
+                {
+                   if (i % 3 == 0)
+                   {
+                        Console.WriteLine(i);
+                   }
+                   
+                }
+            }
+            else if (num > 50 && num < 100)
+            {
+                for (int i = 1; i <= num; i++)
+                {
+                    if (i % 5 == 0)
+                    {
+                         Console.WriteLine(i);
+                    }
+                }
+                    
+                
+            }
+            else if (num > 100)
+            {
+                for (int i = 1; i <= num; i++)
+                {
+                    if (i % 8 == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+
+        }
+
+    }
+}
